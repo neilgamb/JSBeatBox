5c2f148ce4f493d743a7d51c53efcd56f2376e76
Neilson Gamble <neilgamb@Neilsons-MacBook-Pro.local>
2018-11-02 18:57:21 -0400

    hooking up sounds

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 function playSound(e) {
   const audio = document.querySelector(`audio[data-key='${e.keyCode}']`);
   const key = document.querySelector(`div[data-key='${e.keyCode}']`);
+
+  if (!audio) return;
+
+  audio.currentTime = 0;
+  audio.play();
 }
 
 function init() {
